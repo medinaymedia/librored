25280856ff710c573eb2b32fac4ff5343285ac16
Ana <maramedinacrespo@gmail.com>
2025-08-30 21:33:51 +0200

    Initial commit

---
+++ src/main/java/es/codeurjc/librored/LibroredApplication.java
@@ -0,0 +1,13 @@
+package es.codeurjc.librored;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibroredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibroredApplication.class, args);
+	}
+
+}
