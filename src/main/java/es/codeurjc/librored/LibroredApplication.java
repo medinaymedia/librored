f4db30d3986098db5c06425879a86d96c09e0f30
Ana <maramedinacrespo@gmail.com>
2025-08-30 21:36:15 +0200

    Initial config for webapp librored

--- src/main/java/es/codeurjc/librored/LibroredApplication.java
+++ src/main/java/es/codeurjc/librored/LibroredApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LibroredApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LibroredApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LibroredApplication.class, args);
+    }
 
 }
